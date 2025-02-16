55048e699d3b1287f85c5f272d5d1f80e79a7f1e
Bruno Edson <brunoedson2201@gmail.com>
2025-02-15 22:37:47 -0300

    update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 // config.js
-const CAPACIDADE_A = 8;
-const CAPACIDADE_B = 5;
-const CAPACIDADE_C = 3;
+export const CAPACIDADE_A = 8;
+export const CAPACIDADE_B = 5;
+export const CAPACIDADE_C = 3;
 
-const estadoInicial = [8, 0, 0];
-const objetivo = [4, 4, 0];
-
-module.exports = { CAPACIDADE_A, CAPACIDADE_B, CAPACIDADE_C, estadoInicial, objetivo };
+export const estadoInicial = [8, 0, 0];
+export const objetivo = [4, 4, 0];
