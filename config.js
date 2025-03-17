137155a9aceed6af9f8dcab19f5c6a9e960e65d2
Bruno Edson <brunoedson2201@gmail.com>
2025-03-17 18:57:46 -0300

    add pesos

--- config.js
+++ config.js
@@ -3,5 +3,9 @@ export const CAPACIDADE_A = 8;
 export const CAPACIDADE_B = 5;
 export const CAPACIDADE_C = 3;
 
+export const PESO_A = 1;
+export const PESO_B = 2;
+export const PESO_C = 3;
+
 export const estadoInicial = [8, 0, 0];
 export const objetivo = [4, 4, 0];
